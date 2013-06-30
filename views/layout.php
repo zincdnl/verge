9a63409d626b8efc43b6d31b2b08bb711d758e7e
zincdnl@yahoo.co.id <zincdnl@yahoo.co.id>
2013-06-30 20:43:05 +0700

    user master.css

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,9 @@
 <html>
-<body>
-<h1>Verge</h1>
-<?php include($this->content); ?>
-</body>
+	<head>
+		<link href="<?php echo $this->make_route('/css/master.css') ?>" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<h1>Verge</h1>
+		<?php include($this->content); ?>
+	</body>
 </html>
