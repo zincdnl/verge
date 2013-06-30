bdd5f4df442fba899e21b3fe007c63af2c760515
zincdnl@yahoo.co.id <zincdnl@yahoo.co.id>
2013-06-30 20:28:06 +0700

    http://localhost/verge/signup
    will show text box and button

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 include 'lib/bones.php';
 
 get('/', function($app) {
-echo "Home";
+	$app->set('message', 'Welcome Back!');
+	$app->render('home');
 });
 get('/signup', function($app) {
-echo "Signup!";
+	$app->render('signup');
+});
+post('/signup', function($app) {
+	$app->set('message', 'Thanks for Signing Up ' .
+	$app->form('name') . '!');
+	$app->render('home');
 });
--- lib/bones.php
+++ lib/bones.php
@@ -2,19 +2,33 @@
 ini_set('display_errors','On');
 error_reporting(E_ERROR | E_PARSE);
 define('ROOT', __DIR__ . '/..');
+//define('ROOT', dirname(dirname(__FILE__)));
 
 
 function get($route, $callback) {
-	Bones::register($route, $callback);
+	Bones::register($route, $callback, 'GET');
 }
-
+function post($route, $callback) {
+	Bones::register($route, $callback, 'POST');
+}
+function put($route, $callback) {
+	Bones::register($route, $callback, 'PUT');
+}
+function delete($route, $callback) {
+	Bones::register($route, $callback, 'DELETE');
+}
 class Bones {
 	private static $instance;
 	public static $route_found = false;
 	public $route = '';
-
+	public $method = '';
+	public $content = '';
+	public $vars = array();
+	
+	
 	public function __construct() {
 		$this->route = $this->get_route();
+		$this->method = $this->get_method();
 	}
 	public static function get_instance() {
 		if (!isset(self::$instance)) {
@@ -22,13 +36,24 @@ class Bones {
 		}
 		return self::$instance;
 	}
-	public static function register($route, $callback) {
+	public static function register($route, $callback, $method) {
 		$bones = static::get_instance();
-		if ($route == $bones->route && !static::$route_found) {
+		if ($route == $bones->route && !static::$route_found && $bones->method == $method) {
 			static::$route_found = true;
 			echo $callback($bones);
 		} else {
 			return false;
+		}
+	}
+	public function form($key) {
+		return $_POST[$key];
+	}
+	public function make_route($path = '') {
+		$url = explode("/", $_SERVER['PHP_SELF']);
+		if ($url[1] == "index.php") {
+			return $path;
+		} else {
+			return '/' . $url[1] . $path;
 		}
 	}
 	protected function get_route() {
@@ -37,6 +62,23 @@ class Bones {
 			return '/' . $route['request'];
 		} else {
 			return '/';
+		}
+	}
+	protected function get_method() {
+		return isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+	}
+	public function set($index, $value) {
+		$this->vars[$index] = $value;
+	}
+	public function render($view, $layout = "layout") {
+		$this->content = ROOT. '/views/' . $view . '.php';
+		foreach ($this->vars as $key => $value) {
+			$$key = $value;
+		}
+		if (!$layout) {
+			include($this->content);
+		} else {
+			include(ROOT. '/views/' . $layout . '.php');
 		}
 	}
 }
