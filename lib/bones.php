dd44a6f9b3a33651fc339514e4514c4c933a6aba
zincdnl <zincdnl@yahoo.co.id>
2013-06-30 19:40:28 +0700

    http://localhost/verge/ respon HOME http://localhost/verge/signup respon signup

---
+++ lib/bones.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors','On');
+error_reporting(E_ERROR | E_PARSE);
+define('ROOT', __DIR__ . '/..');
+
+
+function get($route, $callback) {
+	Bones::register($route, $callback);
+}
+
+class Bones {
+	private static $instance;
+	public static $route_found = false;
+	public $route = '';
+
+	public function __construct() {
+		$this->route = $this->get_route();
+	}
+	public static function get_instance() {
+		if (!isset(self::$instance)) {
+		self::$instance = new Bones();
+		}
+		return self::$instance;
+	}
+	public static function register($route, $callback) {
+		$bones = static::get_instance();
+		if ($route == $bones->route && !static::$route_found) {
+			static::$route_found = true;
+			echo $callback($bones);
+		} else {
+			return false;
+		}
+	}
+	protected function get_route() {
+		parse_str($_SERVER['QUERY_STRING'], $route);
+		if ($route) {
+			return '/' . $route['request'];
+		} else {
+			return '/';
+		}
+	}
+}
