096a56a95f06b83c888de3605699f8f9b3bb6c74
zincdnl@yahoo.co.id <zincdnl@yahoo.co.id>
2013-07-01 08:19:22 +0700

    'CouchDB Documents can now be created through the signup form using curl.'

--- index.php
+++ index.php
@@ -18,6 +18,22 @@ post('/signup', function($app) {
 	$user->name = $app->form('name');
 	$user->email = $app->form('email');
 	echo json_encode($user);
+	$curl = curl_init();
+// curl options
+	$options = array(
+		CURLOPT_URL => 'localhost:5984/verge',
+		CURLOPT_POSTFIELDS => json_encode($user),
+		CURLOPT_HTTPHEADER => array ("Content-Type:application/json"),
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "utf-8",
+		CURLOPT_HEADER => false,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_AUTOREFERER => true
+	);
+	curl_setopt_array($curl, $options);
+	curl_exec($curl);
+	curl_close($curl);
 	$app->set('message', 'Thanks for Signing Up ' .
 	$app->form('name') . '!');
 	$app->render('home');
